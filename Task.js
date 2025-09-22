c1bd5e934e06bcd734bddd2baaa762395837f17c
OMAAN-JALLANI <omaanjallani5530@gmail.com>
2025-09-22 17:14:26 +0500

    added project files

---
+++ Task.js
@@ -0,0 +1,15 @@
+let btn_org = document.querySelector(".color-img");
+
+function orange() { 
+    btn_org.style.backgroundImage = "url('/Images/orange.png')";
+
+}
+function blue() { 
+    btn_org.style.backgroundImage = "url('/Images/blue.png')";
+
+}
+function white() { 
+    btn_org.style.backgroundImage = "url('/Images/white.png')";
+
+}
+ 
