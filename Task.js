597113246910a85b3e5d333020d33bb88c3bbe84
OMAAN-JALLANI <omaanjallani5530@gmail.com>
2025-09-23 16:45:48 +0500

    save new changes and pic added

--- Task.js
+++ Task.js
@@ -1,15 +1,15 @@
 let btn_org = document.querySelector(".color-img");
 
 function orange() { 
-    btn_org.style.backgroundImage = "url('./Images/orange.png')";
+    btn_org.style.backgroundImage = "url('./Images/orange.webp')";
 
 }
 function blue() { 
-    btn_org.style.backgroundImage = "url('./Images/blue.png')";
+    btn_org.style.backgroundImage = "url('./Images/blue.webp')";
 
 }
 function white() { 
-    btn_org.style.backgroundImage = "url('./Images/white.png')";
+    btn_org.style.backgroundImage = "url('./Images/white.webp')";
 
 }
 
